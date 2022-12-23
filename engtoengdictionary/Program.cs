737a07e7cba133cf936a690e544dfe616eefd633
BerkayCimendag <cimendagberkay@gmail.com>
2022-12-23 08:20:28 +0300

    Add project files.

---
+++ engtoengdictionary/Program.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+Console.WriteLine("Write the word that you are curious about the meaning in English?");
+string word = Console.ReadLine();
+string url = $"https://api.dictionaryapi.dev/api/v2/entries/en/{word}";
+HttpClient client = new HttpClient();
+HttpResponseMessage answer = await client.GetAsync(url);
+string json = await answer.Content.ReadAsStringAsync();
+dynamic data = JsonConvert.DeserializeObject(json);
+Console.WriteLine(data[0].meanings[0].definitions[0].definition);
+
